5dc59ba2de5dbf32885b225afc9c8d86d3fdec85
techyNonso <williamikeji@gmail.com>
2024-04-07 17:20:23 +0100

    wrong page error

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const NoPage = () => {
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate('/');
+    })
     return <h1>404</h1>;
 };
 
